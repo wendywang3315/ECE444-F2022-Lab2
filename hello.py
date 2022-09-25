fca89953eb66d1235d39930d71f1ecfc353e3bde
wendywang3315 <yutong_wang3315@outlook.com>
2022-09-25 14:31:24 -0400

    continue reproducing example 4-7

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 from ensurepip import bootstrap
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
@@ -21,9 +21,11 @@ def index():
     name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ""
-    return render_template("index.html", form=form, name=name)
+        old_name = session.get("name")
+        if old_name is not None and old_name != form.name.data:
+            flash("Looks like you have changed your name!")
+        session["name"] = form.name.data
+    return render_template("index.html", form=form, name=session.get("name"))
 
 @app.route("/user/<name>")
 def user(name):
