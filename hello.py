a6990b47ce01298f4628f03677e33e22502bcdfb
wendywang3315 <yutong_wang3315@outlook.com>
2022-09-25 15:29:30 -0400

    add email field and validator for activity 4

--- hello.py
+++ hello.py
@@ -4,11 +4,12 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Email
 
 class NameForm(FlaskForm):
     name = StringField("What is your name?", validators=[DataRequired()])
+    email = EmailField("What is your UofT Email address?", validators=[DataRequired(), Email()])
     submit = SubmitField("Submit")
 
 app = Flask(__name__)
@@ -22,10 +23,17 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get("name")
+        old_email = session.get("email")
         if old_name is not None and old_name != form.name.data:
             flash("Looks like you have changed your name!")
+        if old_email is not None and old_email != form.email.data:
+            flash("Looks like you have changed your email!")
         session["name"] = form.name.data
-    return render_template("index.html", form=form, name=session.get("name"))
+        session["email"] = form.email.data
+        form.name.data = ""
+        form.email.data = ""
+        return redirect(url_for('index'))
+    return render_template("index.html", form=form, name=session.get("name"), email=session.get("email"))
 
 @app.route("/user/<name>")
 def user(name):
